e883fdae6b42a6034224bd1804f7c67c2ea10441
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-22 20:06:37 +0530

    update code

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,55 +1,89 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+// import React from 'react'
+// import {Editor } from '@tinymce/tinymce-react';
+// import {Controller } from 'react-hook-form';
 
 
-export default function RTE({name, control, label, defaultValue =""}) {
+// export default function RTE({name, control, label, defaultValue =""}) {
+//   return (
+//     <div className='w-full'> 
+//     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+//     <Controller
+//     name={name || "content"}
+//     control={control}
+//     render={({field: {onChange}}) => (
+//         <Editor
+//         initialValue={defaultValue}
+//         init={{
+//             initialValue: defaultValue,
+//             height: 500,
+//             menubar: true,
+//             plugins: [
+//                 "image",
+//                 "advlist",
+//                 "autolink",
+//                 "lists",
+//                 "link",
+//                 "image",
+//                 "charmap",
+//                 "preview",
+//                 "anchor",
+//                 "searchreplace",
+//                 "visualblocks",
+//                 "code",
+//                 "fullscreen",
+//                 "insertdatetime",
+//                 "media",
+//                 "table",
+//                 "code",
+//                 "help",
+//                 "wordcount",
+//                 "anchor",
+//             ],
+//             toolbar:
+//             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+//             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+//         }}
+//         onEditorChange={onChange}
+//         />
+//     )}
+//     />
+
+//      </div>
+//   )
+// }
+
+
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function App() {
   return (
-    <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
+    <Editor
+      apiKey='e5uwm9gwik96tfc83pc1xhp3tn0h20f4mi4iez91kci1t25t'
+      init={{
+        plugins: [
+          // Core editing features
+          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+          // Your account includes a free trial of TinyMCE premium features
+          // Try the most popular premium features until Dec 6, 2024:
+          'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+          // Early access to document converters
+          'importword', 'exportword', 'exportpdf'
+        ],
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+        exportpdf_converter_options: { 'format': 'Letter', 'margin_top': '1in', 'margin_right': '1in', 'margin_bottom': '1in', 'margin_left': '1in' },
+        exportword_converter_options: { 'document': { 'size': 'Letter' } },
+        importword_converter_options: { 'formatting': { 'styles': 'inline', 'resets': 'inline',	'defaults': 'inline', } },
+      }}
+      initialValue="Welcome to TinyMCE!"
     />
-
-     </div>
-  )
+  );
 }
-
